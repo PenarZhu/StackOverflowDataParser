12d3ebfdabe81b419083203d549afab77be77c2d
PenarZhu <fly_bird_1982@hotmail.com>
2017-09-25 12:16:21 +0800

    delete original copy

--- Posts/Writer/SolrIndexConstant.java
+++
@@ -1,8 +0,0 @@
-package StackOverflow.XMLParser.Posts.Writer;
-
-public class SolrIndexConstant {
-	static final String DOC = "doc";
-	static final String ADD = "add";
-	static final String FIELD = "field";
-	static final String NAME = "name";
-}
